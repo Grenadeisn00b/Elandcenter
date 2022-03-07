2a641159e296cb93bb5ad9ef3524a4946bc4d318
Grenadeisn00b <90265596+Grenadeisn00b@users.noreply.github.com>
2022-03-07 17:36:57 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,134 @@
+(function($) {
+
+	$(document).ready(function() {
+	  $('body').addClass('js');
+	  var $menu = $('#menu'),
+	    $menulink = $('.menu-link');
+	  
+	$menulink.click(function() {
+	  $menulink.toggleClass('active');
+	  $menu.toggleClass('active');
+	  return false;
+	});});
+
+
+	videoPopup();
+
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:true,
+	    autoplay:true,
+		autoplayTimeout:5000,
+		autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        550:{
+	            items:2
+	        },
+	        750:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        },
+	        1200:{
+	            items:5
+	        }
+	    }
+	})
+
+
+	$(".Modern-Slider").slick({
+	    autoplay:true,
+	    autoplaySpeed:10000,
+	    speed:600,
+	    slidesToShow:1,
+	    slidesToScroll:1,
+	    pauseOnHover:false,
+	    dots:true,
+	    pauseOnDotsHover:true,
+	    cssEase:'fade',
+	   // fade:true,
+	    draggable:false,
+	    prevArrow:'<button class="PrevArrow"></button>',
+	    nextArrow:'<button class="NextArrow"></button>', 
+	});
+
+
+	$("div.features-post").hover(
+	    function() {
+	        $(this).find("div.content-hide").slideToggle("medium");
+	    },
+	    function() {
+	        $(this).find("div.content-hide").slideToggle("medium");
+	    }
+	 );
+
+
+	$( "#tabs" ).tabs();
+
+
+	(function init() {
+	  function getTimeRemaining(endtime) {
+	    var t = Date.parse(endtime) - Date.parse(new Date());
+	    var seconds = Math.floor((t / 1000) % 60);
+	    var minutes = Math.floor((t / 1000 / 60) % 60);
+	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	    return {
+	      'total': t,
+	      'days': days,
+	      'hours': hours,
+	      'minutes': minutes,
+	      'seconds': seconds
+	    };
+	  }
+	  
+	  function initializeClock(endtime){
+	  var timeinterval = setInterval(function(){
+	    var t = getTimeRemaining(endtime);
+	    document.querySelector(".days > .value").innerText=t.days;
+	    document.querySelector(".hours > .value").innerText=t.hours;
+	    document.querySelector(".minutes > .value").innerText=t.minutes;
+	    document.querySelector(".seconds > .value").innerText=t.seconds;
+	    if(t.total<=0){
+	      clearInterval(timeinterval);
+	    }
+	  },1000);
+	}
+	initializeClock(((new Date()).getFullYear()+1) + "/1/1")
+	})()
+
+})(jQuery);
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
